40318662980269f8c0fd9d1f573bf9aafa38f986
monty <chenmonty@gmail.com>
2022-04-13 16:14:19 +0800

    my basic config

---
+++ custom/chadrc.lua
@@ -0,0 +1,66 @@
+--  S代表Shift键盘；s代表Command键，M或A代表option键盘, C代表Control键盘
+
+-- Just an example, supposed to be placed in /lua/custom/
+
+local userPlugins = require("custom.myplugins.myinit") -- 加载自定义添加的插件 /lua/custom/myplugins/myinit.lua
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+	theme = "onedark",
+}
+
+M.options = {
+	-- custom = {}
+	-- general nvim/vim options , check :h optionname to know more about an option
+
+	clipboard = "", -- 取消共享系统剪贴板。 在输入模式用 s-v 也就是 Command + v 也能从系统剪贴板粘贴到vim
+	tabstop = 4, -- tab 4个空格
+
+	terminal = { -- A-i 浮动终端切换，定义终端显示大小
+		location = {
+			horizontal = "rightbelow",
+			vertical = "rightbelow",
+			float = {
+				relative = "editor",
+				row = 0.3,
+				col = 0.25,
+				width = 0.7,
+				height = 0.7,
+				border = "single",
+			},
+		},
+	},
+}
+
+M.plugins = {
+	install = userPlugins,
+
+	-- enable/disable plugins (false for disable)
+	status = {
+		alpha = true, -- dashboard
+	},
+
+	default_plugin_config_replace = {
+		nvim_treesitter = {
+			-- 增量选择代码块（很好用，打开了）感觉比gcmt/wildfire.vim 好用
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					-- 初始化选择
+					init_selection = "<CR>",
+					-- 递增
+					node_incremental = "<CR>",
+					-- 递减
+					node_decremental = "<BS>",
+					-- 选择一个范围
+					scope_incremental = "<TAB>",
+				},
+			},
+		},
+	},
+}
+
+return M
