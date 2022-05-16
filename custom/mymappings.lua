7ac5301081d99be838e399b10e766fbecf791f89
monty <chenmonty@gmail.com>
2022-05-16 14:39:37 +0800

    根据新版格式重新定义快捷键

--- custom/mymappings.lua
+++ custom/mymappings.lua
@@ -19,26 +19,38 @@ M.nvterm = {
       },
 }
 
-map("n", "<leader>cc", ":Telescope <CR>")
-map("n", "<leader>q", ":q <CR>")
+M.hop = {  -- Hop 字词行快速定位跳转
+    n = {
 
--- 历史修改记录
-map("n", "<A-u>", ":UndotreeToggle<CR>")
-
-
--- Hop 字词行快速定位跳转
-map("n", "<leader>hc", ":HopChar1 <CR>")  -- <leader>hc 输入1个字符,就可以快速跳转到给字符的位置
-map("n", "<leader>hw", ":HopWord <CR>")
-map("n", "<leader>hl", ":HopLine <CR>")
+        ["<leader>hc"] = {"<cmd>HopChar1<CR>", "Hop Char1"},
+        ["<leader>hl"] = {"<cmd>HopLine<CR>", "Hop Line"},
+    },
+}
 
 
+M.untotree={ -- 历史修改记录
+    n = {
+        ["<A-u>"] = {"<cmd>UndotreeToggle<CR>", "Undotree Toggle"},
+    },
+}
 
--- -- 格式化
--- NvChad 的 <leader>fm shortcut 预设只在設妥 lsp server 時才有作用，但 null-ls format 不需要lsp server
-map("n", "<leader>cf", ":lua vim.lsp.buf.formatting_sync() <CR>")
+
+M.null_ls = {   -- NvChad 的 <leader>fm shortcut 预设只在設妥 lsp server 時才有作用，但 null-ls format 不需要lsp server
+    n = {
+        ["<leader>cf"] = {"<cmd>lua vim.lsp.buf.formatting_sync() <CR>", "code format"},
+    },
+}
+
+
 
 -- 不依赖插件的普通映射
-map("n", "<C-a>", "ggVG") -- 全选
--- map("n", "<C-k>", "d$") -- 从当前光标位置一直删除到行尾
+M.general = {
+    n = {
+        ["<C-a>"] = {"ggVG", "select all"},
+        -- map("n", "<C-k>", "d$") -- 从当前光标位置一直删除到行尾
+    },
+}
+
+
 
 return M
